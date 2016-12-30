3521f2b12aa6323b223904dffae04a009e0fa553
Sergio Romero <sergeromero@gmail.com>
2016-12-29 20:54:29 -0500

    Added a tour page and request group rate  page.

--- src/medowlark.js
+++ src/medowlark.js
@@ -26,6 +26,14 @@ app.get('/about', function(req, res){
     });
 });
 
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
 app.use(function(req, res){
     res.status(404);
     res.render('404');
